f2926dddb70832206f34dcee651c7483647bd6b1
Naomi Schettini <>
2018-06-29 16:45:43 -0700

    changed information entry to be on separate view controller

---
+++ ToDoApp/View Controllers/TodoListManagerTableViewCells/TodoListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListTableViewCell.swift
+//  ToDoApp
+//
+//  Created by Naomi Schettini on 6/29/18.
+//  Copyright © 2018 Naomi Schettini. All rights reserved.
+//
+
+import UIKit
+
+class TodoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
